38f12ed03e9036241d45a78bb4b127a9fad8e2e2
ahme1kash <kashifrichy@gmail.com>
2024-06-24 18:51:01 +0530

    Adds cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.options("", cors());
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "");
+    res.header("Access-Control-Allow-Origin", "https://mern-crud-frontend-app.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With, X-CallbackType, Content-Type, Accept");
     res.header("Cache-Control", "no-cache");
